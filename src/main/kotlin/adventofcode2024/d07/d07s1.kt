4daedb2d366ccd22cc27a79b18add268df07b6dc
Jaroslav Klech <jarek.klech@seznam.cz>
2024-12-07 14:14:31 +0100

    Day 7 - not workng

---
+++ src/main/kotlin/adventofcode2024/d07/d07s1.kt
@@ -0,0 +1,75 @@
+package eu.klech.adventofcode2024.d07
+
+import java.io.File
+
+fun main (){
+
+    println("Path: ${File("src${File.separator}main${File.separator}resources${File.separator}" +"1a-sample.txt").absolutePath}")
+
+    val sample = "7a-sample.txt"
+    val data = "7a-data.txt"
+
+    val input = readFile(data)
+    var result:Long = 0
+
+    input.forEach { line ->
+        val lineNumbers = Regex("\\d+").findAll(line).map { it.value.toLong() }.toList()
+        val lineResult = lineNumbers[0]
+        val numbers = lineNumbers.subList(1,lineNumbers.size)
+        if (isValid(lineResult, numbers)) {
+            result += lineResult
+            println("HIT: $lineResult")
+        }
+    }
+
+
+    println("\n\n Result: $result")
+}
+
+
+fun readFile(fileName: String): List<String>
+        = File("src${File.separator}main${File.separator}resources${File.separator}" + fileName).useLines { it.toList() }
+
+fun isValid(goal: Long, numbers: List<Long>): Boolean {
+    println("goal: $goal, numbers: $numbers")
+    val permutations = permutations(numbers.size -1)
+    println("Permutations: $permutations")
+    var result:Long = -1
+    permutations.forEach { perm ->
+        result = numbers[0]
+        for(i in 1.. numbers.lastIndex) {
+            val next = numbers[i]
+            result = when(perm[i-1]){
+                '+' -> result + next
+                '*' -> result * next
+                else -> 0
+            }
+            if (result > goal) {
+                break
+            }
+            if(result == goal) {
+                return true
+            }
+        }
+    }
+
+    return false
+}
+val permutationOperations = listOf('+', '*')
+
+fun permutations(operationsCount: Int): List<List<Char>> {
+    val permutations = mutableListOf<List<Char>>()
+    if (operationsCount == 1) {
+        permutations += permutationOperations.map { listOf(it) }
+        return permutations
+    }
+
+    for (j in 0 .. permutationOperations.lastIndex) {
+        val permReminder = permutations(operationsCount - 1)
+        permReminder.forEach { perm ->
+            permutations.add(listOf(permutationOperations[j]) + perm)
+        }
+    }
+
+    return permutations
+}
