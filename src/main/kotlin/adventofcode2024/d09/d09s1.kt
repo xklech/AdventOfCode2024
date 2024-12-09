5fced022e80708429c92299181626ba6a2a5702b
Jaroslav Klech <jarek.klech@seznam.cz>
2024-12-10 00:21:20 +0100

    D9 solution part 2 - working

--- src/main/kotlin/adventofcode2024/d09/d09s1.kt
+++ src/main/kotlin/adventofcode2024/d09/d09s1.kt
@@ -24,42 +24,72 @@ fun main (){
 fun readFile(fileName: String): String
         = File("src${File.separator}main${File.separator}resources${File.separator}" + fileName).useLines { it.toList() }[0]
 
-fun expand(inputString: String): List<Int> {
-    val list = mutableListOf<Int>()
+fun expand(inputString: String): List<Pair<Int,Int>> {
+    val list = mutableListOf<Pair<Int,Int>>()
     val numList = Regex("\\d").findAll(inputString).map { it.value.toInt() }.toList()
     var blockFile = 0
     numList.forEachIndexed { index, num ->
         if (index % 2 == 0) {
-            list.addAll(generateSequence { blockFile }.take(num))
+            list.add(Pair(blockFile, num))
             blockFile++
         } else {
-            list.addAll(generateSequence { -1 }.take(num))
+            list.add(Pair(-1, num))
         }
     }
     return list
 }
 
-fun rearrange(expanded: List<Int>): List<Int> {
+fun rearrange(expanded: List<Pair<Int,Int>>): List<Pair<Int,Int>> {
     val result = expanded.toMutableList()
-    var searchIndex = 0
-    for (reverseIndex in expanded.lastIndex downTo 0) {
-        val reverseChar = expanded[reverseIndex]
-        if (reverseChar == -1) continue
-        if (searchIndex >= reverseIndex) {
-            return result
-        }
-        for(i in searchIndex..<reverseIndex) {
-            if (expanded[i] == -1) {
-                result[i] = reverseChar
-                result[reverseIndex] = -1
-                searchIndex = i + 1
+    val toOptimize = expanded.filter { it.first != -1 }.sortedBy { it.first }.asReversed()
+    println("To Optimize: $toOptimize")
+    toOptimize.forEach{file ->
+        squashSpaces(result)
+        val fileIndex = result.indexOf(file)
+        for(i in 0..<fileIndex) {
+            if (result[i].first == -1 && result[i].second >= file.second) {
+                val space = result[i]
+                result.removeAt(fileIndex)
+                result.add(fileIndex, Pair(-1, file.second))
+                result.removeAt(i)
+                result.add(i, file)
+                val remainingSpace = space.second - file.second
+                if (remainingSpace > 0) {
+                    result.add(i+1, Pair(-1, remainingSpace))
+                }
                 break
             }
         }
+
     }
     return result
 }
 
-fun computeChecksum(rearranged: List<Int>): Long {
-    return rearranged.filter { it != -1 }.mapIndexed { index, i -> index * i }.sumOf { it.toLong() }
+fun squashSpaces(list: MutableList<Pair<Int,Int>>) {
+    var iMax = list.lastIndex
+    var i = 0
+    while(i<iMax) {
+        if (list[i].first == -1 && list[i+1].first == -1) {
+            val combinedSize = list[i].second+list[i+1].second
+            list.removeAt(i+1)
+            list.removeAt(i)
+            list.add(i, Pair(-1, combinedSize))
+            iMax--
+        }
+        i++
+    }
+}
+
+fun computeChecksum(rearranged: List<Pair<Int,Int>>): Long {
+    var index = 0
+    var result = 0L
+    rearranged.forEach { item ->
+        for(i in 0..<item.second) {
+            if(item.first != -1) {
+                result += item.first*(index)
+            }
+            index++
+        }
+    }
+    return result
 }
